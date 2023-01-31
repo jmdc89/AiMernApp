20ff6fa7b22de682f29f8df8419cd2f857429915
José Manuel Domínguez <68101685+jmdc89@users.noreply.github.com>
2023-01-31 12:13:47 +0300

    Navbar
    Routing and icons

--- client/src/page/CreatePost.jsx
+++ client/src/page/CreatePost.jsx
@@ -1,4 +1,4 @@
-import React from 'react'
+import React from 'react';
 
 const CreatePost = () => {
   return (
@@ -6,4 +6,4 @@ const CreatePost = () => {
   )
 }
 
-export default CreatePost
+export default CreatePost;
