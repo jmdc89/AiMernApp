207f259568a70a7b9f22faa8d408c2f0eb7234d3
José Manuel Domínguez <68101685+jmdc89@users.noreply.github.com>
2023-01-31 14:57:24 +0300

    Update Home.jsx
    RenderCards

--- client/src/page/Home.jsx
+++ client/src/page/Home.jsx
@@ -2,6 +2,18 @@ import React, { useState, useEffect } from 'react';
 
 import { Loader, Card, FormField } from '../components';
 
+const RenderCards = ({ data, title }) => {
+  if (data?.length > 0) {
+    return (
+      data.map((post) => <Card key={post._id} {...post} />)
+    );
+  }
+
+  return (
+    <h2 className="mt-5 font-bold text-[#6469ff] text-xl uppercase">{title}</h2>
+  );
+};
+
 const Home = () => {
 
   const [loading, setLoading] = useState(false);
