f47a83ee075762ee2045a17ef291fccd4fa67946
José Manuel Domínguez <68101685+jmdc89@users.noreply.github.com>
2023-02-01 14:27:52 +0300

    Update CreatePost.jsx
    handleSubmit

--- client/src/page/CreatePost.jsx
+++ client/src/page/CreatePost.jsx
@@ -17,9 +17,46 @@ const CreatePost = () => {
   const [generatingImg, setGeneratingImg] = useState(false);
   const [loading, setLoading] = useState(false);
 
+  const handleSubmit = async (e) => {
+    e.preventDefault();
+
+    if (form.prompt && form.photo) {
+      setLoading(true);
+      try {
+        const response = await fetch('https://dalle-arbb.onrender.com/api/v1/post', {
+          method: 'POST',
+          headers: {
+            'Content-Type': 'application/json',
+          },
+          body: JSON.stringify({ ...form }),
+        });
+
+        await response.json();
+        alert('Success');
+        navigate('/');
+      } catch (err) {
+        alert(err);
+      } finally {
+        setLoading(false);
+      }
+    } else {
+      alert('Please generate an image with proper details');
+    }
+  };
   
   return (
-    <div>CreatePost</div>
+    <section className="max-w-7xl mx-auto">
+
+      <div>
+        <h1 className="font-extrabold text-[#222328] text-[32px]">Create</h1>
+        <p className="mt-2 text-[#666e75] text-[14px] max-w-[500px]">Generate an imaginative image through DALL-E AI and share it with the community</p>
+      </div>
+
+      <form className="mt-16 max-w-3xl" onSubmit={handleSubmit}>
+
+      </form>
+
+    </section>
   )
 }
 
